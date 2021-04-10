1ab8c0eac73ae5e6925993288fe32ae2aadb4d1c
chisino <82245270+chisino@users.noreply.github.com>
2021-04-09 20:56:42 -0400

    Add files via upload

---
+++ rockpaper.py
@@ -0,0 +1,43 @@
+# This program is a 2-player rock paper scissors game
+
+again = 'y'
+
+while (again == 'y'):
+
+    p1 = input("Player 1 --> Rock, Paper, or Scissors? ")
+    p1 = p1.lower()
+
+    print()
+
+    p2 = input("Player 2 --> Rock, Paper, or Scissors? ")
+    p2 = p2.lower()
+
+    print()
+
+    if (p1 == "rock"):
+        if (p2 == "rock"):
+            print("The game is a draw")
+        elif (p2 == "paper"):
+            print("Player 2 wins!")
+        elif (p2 == "scissors"):
+            print("Player 1 wins!")
+    elif (p1 == "paper"):
+        if (p2 == "rock"):
+            print("Player 1 wins!")
+        elif (p2 == "paper"):
+            print("The game is a draw")
+        elif (p2 == "scissors"):
+            print("Player 2 wins!")
+    elif (p1 == "scissors"):
+        if (p2 == "rock"):
+            print("Player 2 wins!")
+        elif (p2 == "paper"):
+            print("Player 1 wins!")
+        elif (p2 == "scissors"):
+            print("The game is a draw")
+    else:
+        print("Invalid input, try again")
+
+    again = input("Type y to play again, anything else to stop: ")
+
+    print()
